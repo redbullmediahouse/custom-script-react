2a3f3137c95e46e00307c1c292c43f2a0e9e5214
Stefan Hofer <shofer@users.noreply.github.com>
2020-10-16 08:57:07 +0200

    fix(webpack) remove AMD module name
    If you want to use a custom script with requirejs you have to know the module name beforehand.
    In addition to that, it should also be unique, which we can't ensure.
    
    Ref: RBCOM-2002

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ module.exports = {
     entry: './src/main/index.js',
     mode: 'development',
     output: {
-        library: 'customScript',
         libraryTarget: 'amd',
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
